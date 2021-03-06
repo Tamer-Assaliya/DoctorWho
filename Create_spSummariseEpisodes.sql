2cd3795d6041d20b4b0d8924115470b3e1410e23
Tamer <tamer.assaliya@gmail.com>
2021-03-06 10:05:48 +0200

    Refactor some other code

--- Create_spSummariseEpisodes.sql
+++ Create_spSummariseEpisodes.sql
@@ -2,14 +2,14 @@ DROP PROCEDURE IF EXISTS spSummariseEpisodes;
 GO
 CREATE PROCEDURE spSummariseEpisodes
 AS BEGIN
-  SELECT TOP(3) c.CompanionName, COUNT(c.CompanionId) Companion_count
+  SELECT TOP(3) c.CompanionName, COUNT(*) Companion_count
   FROM tblEpisodeCompanion ec
   JOIN tblCompanion c
   ON ec.CompanionId=c.CompanionId
   GROUP BY c.CompanionId , c.CompanionName
   ORDER BY Companion_count DESC 
   ---
-  SELECT TOP(3) e.EnemyName, COUNT(e.EnemyName) Enemy_count
+  SELECT TOP(3) e.EnemyName, COUNT(*) Enemy_count
   FROM tblEpisodeEnemy ee
   JOIN tblEnemy e
   ON ee.EnemyId=e.EnemyId
